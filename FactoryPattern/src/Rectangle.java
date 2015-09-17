cb55991b594cf3d305aa49918fde41b1d0c66ed9
sharonrussell <sharon.russell@comparethemarket.com>
2015-09-17 13:39:49 +0100

    Delete everything!

--- FactoryPattern/src/Rectangle.java
+++
@@ -1,8 +0,0 @@
-public class Rectangle implements Shape
-{
-    @Override
-    public void draw()
-    {
-        System.out.println("Inside Rectange::draw() method.");
-    }
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Java
-Practising Java
